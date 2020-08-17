3373ba38f5b2ac11ad6a0dd6100c0990b4432690
Supr_Inc <dsuprunsw@gmail.com>
2020-08-16 20:42:27 -0400

    Initial Commit

---
+++ BackgroundBLE/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BackgroundBLE
+//
+//  Created by D S on 8/16/20.
+//  Copyright © 2020 Supr. Inc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
