5251f50251ba33ac606ac5332d5229a92658609c
Supr_Inc <dsuprunsw@gmail.com>
2020-08-17 01:36:04 -0400

    Syntax changes.

--- BackgroundBLE/Models/BLEModel.swift
+++ BackgroundBLE/Models/BLEModel.swift
@@ -53,7 +53,6 @@ class BLEModel : NSObject, CBCentralManagerDelegate, CBPeripheralManagerDelegate
     }
     override init() {
         
-        // configure peripheral mode
         self.myUUID = CBUUID(string: "71DA3FD1-7E10-41C1-B16F-4430B506CDE7")
         self.myCharacteristic = CBMutableCharacteristic(type: self.myUUID, properties: [.notify, .read], value: nil, permissions: [.readable])
         
@@ -92,14 +91,14 @@ class BLEModel : NSObject, CBCentralManagerDelegate, CBPeripheralManagerDelegate
     
     func centralManagerDidUpdateState(_ central: CBCentralManager) {
         switch central.state {
-        case CBManagerState.poweredOn:
-            print("Bluetooth is powered on")
-        case CBManagerState.unauthorized:
-            print("Unauthorized for BLE")
-        case CBManagerState.poweredOff:
-            print("Powered Off from central manager")
-        default:
-            print("other error encountered")
+            case CBManagerState.poweredOn:
+                print("central is powered on")
+            case CBManagerState.unauthorized:
+                print("Unauthorized for BLE")
+            case CBManagerState.poweredOff:
+                print("Powered Off from central manager")
+            default:
+                print("other error encountered")
         }
     }
     func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral,advertisementData: [String : Any], rssi RSSI: NSNumber) {
