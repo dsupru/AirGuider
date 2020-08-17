ee62ddba8a8d5a0c036284745b1f1937cbed592c
Supr_Inc <dsuprunsw@gmail.com>
2020-08-16 21:45:18 -0400

    commit 3 files

---
+++ BackgroundBLE/Models/BLEController.swift
@@ -0,0 +1,16 @@
+//
+//  BLEController.swift
+//  BackgroundBLE
+//
+//  Created by D S on 8/16/20.
+//  Copyright © 2020 Supr. Inc. All rights reserved.
+//
+
+import Foundation
+import CoreBluetooth
+
+class BLEController {
+    var PeripheralManager: CBPeripheralManager!
+    var CentralManager: CBCentralManager!
+    
+}
