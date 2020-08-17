8f21d8498ca307174c7b7d97552102d9eb2277dd
Supr_Inc <dsuprunsw@gmail.com>
2020-08-16 22:33:41 -0400

    Change init to configure central and peripheral modes.

--- BackgroundBLE/Models/BLEModel.swift
+++ BackgroundBLE/Models/BLEModel.swift
@@ -10,14 +10,28 @@ import Foundation
 import CoreBluetooth
 
 class BLEModel {
-    var peripheralManager: CBPeripheralManager!
+    
     var centralManager: CBCentralManager!
     var peripherals: [CBPeripheral] = []
     
+    var peripheralManager: CBPeripheralManager!
+    var myUUID: CBUUID
+    var myService: CBMutableService
+    var myCharacteristic: CBMutableCharacteristic
     
     init() {
+        // configure central mode
         self.peripheralManager = CBPeripheralManager(delegate: nil, queue: nil)
         self.centralManager = CBCentralManager(delegate: nil, queue: nil)
+        
+        // configure peripheral mode
+        self.myUUID = CBUUID(string: "71DA3FD1-7E10-41C1-B16F-4430B506CDE7")
+        self.myCharacteristic = CBMutableCharacteristic(type: self.myUUID, properties: [.notify, .read], value: nil, permissions: [.readable])
+        
+        self.myService = CBMutableService(type: self.myUUID, primary: true)
+        self.myService.characteristics = [self.myCharacteristic]
+        self.peripheralManager.add(self.myService)
+        
     }
     
     func startScan() {
